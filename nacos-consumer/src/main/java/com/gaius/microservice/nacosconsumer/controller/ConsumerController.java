98326e55fe5cd9b774a0c1ff702338ec9bbf00b8
bo <zhaobo1@lixin360.com>
2018-12-03 17:17:00 +0800

    feat:nacos server register and discovery

---
+++ nacos-consumer/src/main/java/com/gaius/microservice/nacosconsumer/controller/ConsumerController.java
@@ -0,0 +1,23 @@
+package com.gaius.microservice.nacosconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author
+ * @date
+ */
+@RestController
+public class ConsumerController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
+    public String echo(@PathVariable String str) {
+        return restTemplate.getForObject("http://nacos-client/echo/" + str, String.class);
+    }
+}
